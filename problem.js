80e9cf2e5bf99c01a30fd01e5cb301cbbd9744f7
godakes <joonas.yoon@gmail.com>
2016-02-07 13:52:20 +0000

    Apply markdown on highlighted text

--- problem.js
+++ problem.js
@@ -50,6 +50,8 @@ module.exports = function (req, res, next) {
       else {
         // var description = problemObject.description;
         var description = problemObject._snippetResult.description.value;
+        description = description.replace(/<strong>/g, ' *').replace(/<\/strong>/g, '* ');
+        description = description.replace(/\s{2,}/g, ' ').replace(/(\r\n|\n){2,}/g, '\n');
         var descriptLimit = 250;
         
         var botPayload = {
@@ -60,7 +62,7 @@ module.exports = function (req, res, next) {
                   title: problemObject.id + '번 - ' + problemObject.title,
                   title_link: 'https://www.acmicpc.net/problem/' + problemObject.id,
                   text:
-                    description.replace(/<\/?strong>/g, '*').substr(0, descriptLimit)+(description.length > descriptLimit ? '...':'')
+                    description.substr(0, descriptLimit)+(description.length > descriptLimit ? '...':'')
                     + '\n\n<' + encodeURI('https://www.acmicpc.net/search#q=' + searchQuery + '&c=Problems')
                     + '|검색 결과 더보기 ('+ getResults.nbHits + '개)>',
                   color: "#9b59b6",
