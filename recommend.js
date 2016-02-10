01e013c43d0d092e80c55ec3a1ca4df5ad39ecab
godakes <joonas.yoon@gmail.com>
2016-02-10 08:59:30 +0000

    update

--- recommend.js
+++ recommend.js
@@ -20,21 +20,26 @@ module.exports = function (req, res, next) {
     var botPayload = null;
     var getPid = $("table tr td a[href]");
     var getStar = $("table tr td .bg .fg");
-    var getStarStyle = (getStar[0]).attribs.style.toString();
-    getStarStyle = getStarStyle.match(/width\:([0-9]+)/i);
-    
-    var getProblemUrl = getPid[0].attribs.href;
-    var getStarSize = 0;
-    if(getStarStyle != null) getStarSize = parseInt(getStarStyle[1])*.5;
+    var result = '';
+    for(var i=0; i<3; ++i){
+      var getStarStyle = (getStar[i]).attribs.style.toString();
+      getStarStyle = getStarStyle.match(/width\:([0-9]+)/i);
+      
+      var getProblemUrl = getPid[i].attribs.href;
+      var getStarSize = 0;
+      if(getStarStyle != null) getStarSize = parseInt(getStarStyle[1])*.5;
+      
+      result += getProblemUrl +' (★ '+ getStarSize +'/100)\n';
+    }
     
     var botPayload = {
       attachments: [
           {
               fallback: user_name+'님을 위한 문제 추천',
               pretext: '이 문제를 풀어보세요!',
-              title: getProblemUrl +' (난이도 '+ getStarSize +'/100)',
-              title_link: getProblemUrl,
+              text: result + '\n\n<' + url +'|문제 추천 더 보기>',
               color: "#9b59b6",
+              mrkdwn_in: ["text"]
           }
       ],
     };
